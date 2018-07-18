56511442bfcbe008b2cddbae1d1f00d21775bac3
Chris Lattman <natgeo13@gmail.com>
2018-07-18 16:36:59 -0400

    added solutions

---
+++ VisaCodeSprint/TorqueAndProduct.java
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/contests/visa-codesprint/challenges/torque-and-product
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        for (long i = (long) Math.sqrt(n); i > 0; i--) {
+            if (n % (i * i) == 0) {
+                System.out.printf("%d ", n / (i * i));
+            }
+        }
+        in.close();
+    }
+}
