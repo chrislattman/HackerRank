7f724418793c206fbd427abb12d004885232d8ff
Chris Lattman <natgeo13@gmail.com>
2018-07-20 01:20:09 -0400

    refined code

--- GoldmanSachsCodeSprint/TraderProfit.java
+++ GoldmanSachsCodeSprint/TraderProfit.java
@@ -10,23 +10,17 @@ public class Solution {
 
     static int traderProfit(int k, int n, int[] A) {
         // Complete this function
-        int[][] profit = new int[k + 1][n + 1];
-        for (int i = 0; i <= k; i++) {
-            profit[i][0] = 0;
-        }
-        for (int j = 0; j <= n; j++) {
-            profit[0][j] = 0;
-        }
+        int[][] profits = new int[k + 1][n + 1];
         for (int i = 1; i <= k; i++) {
             for (int j = 1; j < n; j++) {
-                int max_so_far = 0;
+                int max = 0;
                 for (int m = 0; m < j; m++) {
-                    max_so_far = Math.max(max_so_far, A[j] - A[m] + profit[i - 1][m]);
+                    max = Math.max(max, A[j] - A[m] + profits[i - 1][m]);
                 }
-                profit[i][j] = Math.max(profit[i][j - 1], max_so_far);
+                profits[i][j] = Math.max(profits[i][j - 1], max);
             }
         }
-        return profit[k][n - 1];
+        return profits[k][n - 1];
     }
 
     public static void main(String[] args) {
