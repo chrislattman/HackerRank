c0a8a8fb764e991269e10d640c1db415ff409bd7
Chris Lattman <natgeo13@gmail.com>
2018-07-22 00:09:11 -0400

    made minor changes

--- EvenOddQuery.cpp
+++ EvenOddQuery.cpp
@@ -10,14 +10,19 @@ int main()
     cin >> N;
 
     vector<int> A(N);
-    for (int i=0;i<N;i++) cin >> A[i];
+    for (int i = 0; i < N; i++) 
+        cin >> A[i];
 
     int q, x, y;
     cin >> q;
-    for (int i=0;i<q;i++)   {
+    for (int i = 0; i < q; i++)   {
         cin >> x >> y;
-        cout << (((x==y && A[x-1]%2==0) || 
-        (A[x-1]%2==0 && A[x]!=0)) ? "Even" : "Odd") << "\n";
+        if ((x == y && A[x-1] % 2 == 0) || (A[x-1] % 2 == 0 && A[x]!= 0)) {
+            cout << "Even\n";
+        }
+        else {
+            cout << "Odd\n";
+        }
     }
     
     return 0;
