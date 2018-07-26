4e1bb86180b18405700f43783bdc012796031279
Chris Lattman <natgeo13@gmail.com>
2018-07-26 14:59:35 -0400

    added solutions

--- TileStackingProblem.java
+++ TileStackingProblem.java
@@ -15,18 +15,18 @@ public class Solution {
         long[][] dp = new long[m + 1][n + 1];
         long sum = 0;
         dp[0][0] = 1;
-        for (int a = 0; a <= k; a++){
+        for (int a = 0; a <= k; a++) {
             dp[1][a] = 1;
         }
-        for (int b = 1; b < m; b++){
+        for (int b = 1; b < m; b++) {
             sum = 0;
-            for (int c = 0; c <= k; c++){
+            for (int c = 0; c <= k; c++) {
                 sum += dp[b][c];
                 dp[b + 1][c] = sum % 1000000007;
             }
         }
-        for (int d = 2; d <= m; d++){
-            for (int e = k + 1; e <= n; e++){
+        for (int d = 2; d <= m; d++) {
+            for (int e = k + 1; e <= n; e++) {
                 long val = dp[d][e - 1] - dp[d - 1][e - k - 1] + dp[d - 1][e];
                 val %= 1000000007;
                 while (val < 0) {
